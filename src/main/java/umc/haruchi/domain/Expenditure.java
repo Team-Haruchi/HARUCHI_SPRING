0db3ba903e6088e98a5c7284c3bc1f23a2cf0953
ssongmina <thdalsdk5498@naver.com>
2024-07-17 16:05:17 +0900

    [FEAT] entity 설계

---
+++ src/main/java/umc/haruchi/domain/Expenditure.java
@@ -0,0 +1,31 @@
+package umc.haruchi.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.haruchi.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Expenditure extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "bigint default 0")
+    private Long expenditureAmount;
+
+    @Column(nullable = false)
+    private Long expenditureCategory;
+
+    @Column(nullable = true, length = 10)
+    private String memo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "day_budget_id")
+    private DayBudget dayBudget;
+
+}
