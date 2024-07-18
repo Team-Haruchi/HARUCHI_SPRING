cd4035f500c0cde9b2bd7d0948582f82fe9fe7f8
hcg0127 <kej431003@gmail.com>
2024-07-18 11:26:26 +0900

    [FEAT] HaruchiApplication.java 어노테이션 추가

--- src/main/java/umc/haruchi/HaruchiApplication.java
+++ src/main/java/umc/haruchi/HaruchiApplication.java
@@ -2,10 +2,12 @@ package umc.haruchi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableJpaAuditing
 public class HaruchiApplication {
 
 	public static void main(String[] args) {
