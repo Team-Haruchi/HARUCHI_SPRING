41b020f99d42512ca7e9bf73d0c643c70954622e
hcg0127 <kej431003@gmail.com>
2024-07-19 20:28:32 +0900

    [FEAT] 이메일 인증 방식 회원가입 API 구현

---
+++ src/main/java/umc/haruchi/web/controller/MemberApiController.java
@@ -0,0 +1,74 @@
+package umc.haruchi.web.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import umc.haruchi.apiPayload.ApiResponse;
+import umc.haruchi.apiPayload.code.status.SuccessStatus;
+import umc.haruchi.converter.MemberConverter;
+import umc.haruchi.domain.Member;
+import umc.haruchi.service.MemberService;
+import umc.haruchi.web.dto.MemberRequestDTO;
+import umc.haruchi.web.dto.MemberResponseDTO;
+
+import java.io.UnsupportedEncodingException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    @Operation(summary = "회원가입 API", description = "이메일 인증으로 회원가입을 진행하는 API")
+    public ApiResponse<MemberResponseDTO.MemberJoinResultDTO> join(@Valid @RequestBody MemberRequestDTO.MemberJoinDTO request) throws Exception {
+        Member member = memberService.joinMember(request);
+        return ApiResponse.onSuccess(MemberConverter.toJoinResultDTO(member));
+    }
+
+    @PostMapping("/signup/email")
+    @Operation(summary = "이메일 인증 요청 API", description = "이메일에 인증 번호를 보내는 API")
+    @Parameters({
+            @Parameter(name = "email", description = "인증을 받을 메일 주소")
+    })
+    public ApiResponse<MemberResponseDTO> sendEmail(@Valid @RequestParam("email") String email) throws Exception {
+        memberService.sendSimpleMessage(email);
+        return ApiResponse.onSuccess(null);
+    }
+
+    @PostMapping("/signup/email/verify")
+    @Operation(summary = "이메일 인증 확인 API", description = "이메일 인증 번호를 확인하는 API")
+    @Parameters({
+            @Parameter(name = "email", description = "인증을 받을 메일 주소"),
+            @Parameter(name = "code", description = "받은 인증 코드")
+    })
+    public ApiResponse<MemberResponseDTO> verifyEmail(@Valid @RequestParam("email") String email,
+                                                      @RequestParam("code") String code) throws Exception {
+        String authCode = memberService.getVerificationCode(email);
+        memberService.verificationEmail(code, authCode);
+        return ApiResponse.onSuccess(null);
+    }
+
+    @PostMapping("/login")
+    @Operation(summary = "로그인 API", description = "로그인을 진행하는 API")
+    public ApiResponse<MemberResponseDTO.MemberJoinResultDTO> login(@Valid @RequestBody MemberRequestDTO member) {
+        return null;
+    }
+
+    @PostMapping("/logout")
+    @Operation(summary = "로그아웃 API", description = "로그아웃을 진행하는 API")
+    public ApiResponse<MemberResponseDTO.MemberJoinResultDTO> logout(@Valid @RequestBody MemberRequestDTO member) {
+        return null;
+    }
+
+    @PostMapping("/delete")
+    @Operation(summary = "회원탈퇴 API", description = "회원탈퇴를 진행하는 API")
+    public ApiResponse<MemberResponseDTO.MemberJoinResultDTO> deleteMember(@Valid @RequestBody MemberRequestDTO member) {
+        return null;
+    }
+}
