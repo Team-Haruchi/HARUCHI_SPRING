4d8336828a3cd47cb7be09096287bc4c05975f75
ssongmina <thdalsdk5498@naver.com>
2024-07-23 12:03:31 +0900

    [FEAT] 날짜별 예산 금액 조회 API 구현

--- src/main/java/umc/haruchi/converter/DayBudgetConverter.java
+++ src/main/java/umc/haruchi/converter/DayBudgetConverter.java
@@ -2,13 +2,19 @@ package umc.haruchi.converter;
 
 import umc.haruchi.web.dto.DayBudgetResponseDTO;
 
+import java.util.List;
+
 public class DayBudgetConverter {
+
     public static DayBudgetResponseDTO.getDayBudget toGetDayBudget(Integer todayBudget) {
         return DayBudgetResponseDTO.getDayBudget.builder()
                 .dayBudget(todayBudget)
                 .build();
     }
 
-
-
+    public static DayBudgetResponseDTO.getBudget toGetBudget(List<Integer> allBudget) {
+        return DayBudgetResponseDTO.getBudget.builder()
+                .budget(allBudget)
+                .build();
+    }
 }
